6e62a1dbb8e516a1eeab6380c3d18c8592a5f1dd
hazimgharib21 <hazimgharib21@gmail.com>
2023-08-24 21:43:11 +0800

    added serial com

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "motor.h"
 #include "constants.h"
+#include "serial.h"
 
 #define SOP '<'
 #define EOP '>'
@@ -14,15 +15,10 @@ long int interval = 10;
 long int led_interval = 50;
 long int lastMillis = 0;
 long int led_lastMillis = 0;
+long int last_command_receive = 0;
+long int command_data_timeout = 0;
+int consecutive_command_data = 0;
 bool led_state = false;
-
-bool data_in_started = false;
-bool data_in_ended = false;
-char in_data_stream[255];
-int in_data_index = 0;
-int commandSpeed[4];
-int motor_command_index = 0;
-long int last_receive = 0;
 
 void setup() {
   Serial.begin(1000000);
@@ -34,106 +30,110 @@ void setup() {
   RRMotor.init();
   RLMotor.init();
   lastMillis = millis();
+  last_command_receive = millis();
   led_lastMillis = millis();
   pinMode(LED_BUILTIN, OUTPUT);
-  commandSpeed[0] = 0;
-  commandSpeed[1] = 0;
-  commandSpeed[2] = 0;
-  commandSpeed[3] = 0;
 }
 
 void loop() {
   long currentMillis = millis();
+  command_data_timeout = currentMillis - last_command_receive;
   FRMotor.update();
   FLMotor.update();
   RRMotor.update();
   RLMotor.update();
 
-  while(Serial.available() > 0)
+  if(command_data_timeout > 100)
   {
-   char inChar = Serial.read();
-    if(inChar == SOP)
-    {
-      data_in_started = true;
-      data_in_ended = false;
-      in_data_index = 0;
-   }
-    else if(inChar == EOP || inChar == '\n')
-    {
-      data_in_started = false;
-      data_in_ended = true;
-      in_data_index = 0;
-      motor_command_index = 0;
+    FRMotor.move(0);
+    FLMotor.move(0);
+    RRMotor.move(0);
+    RLMotor.move(0);
+    consecutive_command_data = 0;
+  }
 
-      FRMotor.move(commandSpeed[0]);
-      FLMotor.move(commandSpeed[1]);
-      RRMotor.move(commandSpeed[2]);
-      RLMotor.move(commandSpeed[3]);
-      break;
-    }
-    else
+  if(Serial.available() > 0)
+  {
+    char buffer[255];
+    int inBufferSize;
+    inBufferSize = Serial.readBytesUntil('\n', buffer, 255);
+
+    bool start_data = false;
+    bool end_data = false;
+    bool command_data = false;
+    int in_data_index = 0;
+    char in_data_stream[255];
+    int commandSpeed[4] = {0,0,0,0};
+    int motor_command_index = 0;
+
+    for(uint8_t i = 0; i < inBufferSize; i++)
     {
-      if(data_in_started && !data_in_ended)
+      if(buffer[i] == SOP)
       {
-        if(inChar == ',')
+        start_data = true;
+        end_data = false;
+
+      }
+      else if(buffer[i] == EOP)
+      {
+        start_data = false;
+        end_data = true;
+        if(consecutive_command_data > 10)
         {
-          char *pNext;
-          commandSpeed[motor_command_index] = strtol(in_data_stream, &pNext, 10);
-          in_data_index = 0;
-          motor_command_index++;
-          for(uint8_t i = 0; i < 10; i++)
-          {
-            in_data_stream[i] = 0;
-          }
+          FRMotor.move(commandSpeed[0]);
+          FLMotor.move(commandSpeed[1]);
+          RRMotor.move(commandSpeed[2]);
+          RLMotor.move(commandSpeed[3]);
+          consecutive_command_data = 10;
         }
-        else if(inChar == 'r')
-        {
-          data_in_started = false;
-          data_in_ended = true;
-          in_data_index = 0;
-          motor_command_index = 0;
-          Serial.print('<');
-          Serial.print(FRMotor.counter());
-          Serial.print(',');
-          Serial.print(FLMotor.counter());
-          Serial.print(',');
-          Serial.print(RRMotor.counter());
-          Serial.print(',');
-          Serial.print(RLMotor.counter());
-          Serial.print(',');
-          Serial.print(last_receive);
-          Serial.println('>');
-          break;
-        }
-        else
-        {
-          in_data_stream[in_data_index] = inChar;
-          in_data_index++;
-        }
+       break;
       }
       else
       {
+        if(start_data && !end_data)
+        {
+          if(buffer[i] == 'r')
+          {
+            start_data = false;
+            end_data = true;
+            Serial.print('<');
+            Serial.print(FRMotor.counter());
+            Serial.print(',');
+            Serial.print(FLMotor.counter());
+            Serial.print(',');
+            Serial.print(RRMotor.counter());
+            Serial.print(',');
+            Serial.print(RLMotor.counter());
+            Serial.println('>');
+            break;
+          }
+          else if(buffer[i] == ',')
+          {
+            char *pNext;
+            commandSpeed[motor_command_index] = strtol(in_data_stream, &pNext, 10);
+            in_data_index = 0;
+            motor_command_index++;
+            for(uint8_t j = 0; j < 255; j++)
+            {
+              in_data_stream[j] = 0;
+            }
+          }
+          else
+          {
+            command_data = true;
+            in_data_stream[in_data_index] = buffer[i];
+            in_data_index++;
+          }
+        }
 
       }
     }
+    if(command_data)
+    {
+      last_command_receive = currentMillis;
+      consecutive_command_data++;
+    }
   }
-
-/*
-  if((currentMillis - lastMillis) >= interval)
-  {
-    lastMillis = currentMillis;
-    Serial.print('<');
-    Serial.print(FRMotor.counter());
-    Serial.print(',');
-    Serial.print(FLMotor.counter());
-    Serial.print(',');
-    Serial.print(RRMotor.counter());
-    Serial.print(',');
-    Serial.print(RLMotor.counter());
-    Serial.println('>');
-
-  }
-  */
 
   if((currentMillis - led_lastMillis) >= led_interval)
   {
